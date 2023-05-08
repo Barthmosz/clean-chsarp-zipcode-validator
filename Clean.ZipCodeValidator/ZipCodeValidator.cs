399201c245d8c35f232e262c23ec9b3b580e54a5
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-05-08 20:15:31 -0300

    feat: ensure ZipCodeValidator validates properly

---
+++ Clean.ZipCodeValidator/ZipCodeValidator.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Clean.ZipCodeValidator
+{
+    public static class ZipCodeValidator
+    {
+        public static bool Validate(string zipCode)
+        {
+            string zipCodeRegex = "^\\d{5}$";
+            return Regex.IsMatch(zipCode, zipCodeRegex);
+        }
+    }
+}
