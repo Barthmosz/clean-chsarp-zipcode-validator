891365f6b742b0ce59196112a7cbd4ab13aa1138
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-05-08 20:52:26 -0300

    test: ensure ZipCodeValidator returns false if an empty zip code is provided

--- Clean.ZipCodeValidator.Test/ZipCodeValidatorTest.cs
+++ Clean.ZipCodeValidator.Test/ZipCodeValidatorTest.cs
@@ -10,5 +10,11 @@ namespace Clean.ZipCodeValidator.Test
         {
             Assert.That(ZipCodeValidator.Validate("1234"), Is.EqualTo(false));
         }
+
+        [Test, Description("should return false if an empty zip code is provided")]
+        public void ZipCodeValidator_EmptyZipCode_ReturnsFalse()
+        {
+            Assert.That(ZipCodeValidator.Validate(""), Is.EqualTo(false));
+        }
     }
 }
