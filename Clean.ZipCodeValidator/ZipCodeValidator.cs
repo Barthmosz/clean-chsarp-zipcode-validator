6df942af80fdb1ad57caf3ac0dac5b19dde1aa5b
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-05-08 20:53:55 -0300

    docs: add docstring to ZipCodeValidator

--- Clean.ZipCodeValidator/ZipCodeValidator.cs
+++ Clean.ZipCodeValidator/ZipCodeValidator.cs
@@ -4,6 +4,11 @@ namespace Clean.ZipCodeValidator
 {
     public static class ZipCodeValidator
     {
+        /// <summary>
+        /// Validates a zip code.
+        /// </summary>
+        /// <param name="zipCode"></param>
+        /// <returns></returns>
         public static bool Validate(string zipCode)
         {
             string zipCodeRegex = "^\\d{5}$";
