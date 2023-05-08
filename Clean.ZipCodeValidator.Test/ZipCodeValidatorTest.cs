7b7da519020177b82a628c13e08576a87ba353a1
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-05-08 20:52:04 -0300

    test: ensure ZipCodeValidator returns false if an invalid zip code is provided

---
+++ Clean.ZipCodeValidator.Test/ZipCodeValidatorTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Clean.ZipCodeValidator.Test
+{
+    [TestFixture]
+    public class ZipCodeValidatorTest
+    {
+        [Test, Description("should return false if an invalid zip code is provided")]
+        public void ZipCodeValidator_InvalidZipCode_ReturnsFalse()
+        {
+            Assert.That(ZipCodeValidator.Validate("1234"), Is.EqualTo(false));
+        }
+    }
+}
